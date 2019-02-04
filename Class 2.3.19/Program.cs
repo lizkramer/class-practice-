1381ff5f04769c8cc8449355095a56c9f07ba862
lakra <lakra@DESKTOP-SS1186V>
2019-02-03 20:16:29 -0600

    Add project files.

---
+++ Class 2.3.19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_2._3._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Class Jan 23/Class Jan 23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Class_Jan_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            string standardGreeting = "Hello, " + name + "!";
+          
+            if(name == "Amar")
+            {
+                Console.WriteLine(standardGreeting + " I like your shoes.");
+            }
+
+            else if(name == "Brandy")
+            {
+                Console.WriteLine(standardGreeting + " You seem like a cool person.");
+            }
+            else
+            {
+                Console.WriteLine(standardGreeting);
+            }
+
+            Console.ReadLine();
+ 
+        }
+    }
+}
